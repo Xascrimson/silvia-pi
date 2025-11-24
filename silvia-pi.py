6c0a31acb1440505eec119207b42a6191c8b4d4b
Xascrimson <35974973+Xascrimson@users.noreply.github.com>
2025-11-24 22:51:59 +1100

    negation

--- silvia-pi.py
+++ silvia-pi.py
@@ -25,7 +25,7 @@ def he_control_loop(dummy, state,timeState):
 
             avgpid = state['avgpid']
             
-            if not state['awake'] or state['circuitBreaker'] or state['wakeup']:
+            if not state['awake'] or state['circuitBreaker'] or not state['wakeup']:
                 state['heating'] = False
                 GPIO.output(conf.he_pin, 0)
                 sleep(1)
